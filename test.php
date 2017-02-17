bf7beaf6a64e8d831d7c9c81e703b21efb080764
Hi-Joe <hgsoft@yeah.net>
2017-02-17 20:03:34 +0800

    Update test.php
    优化更新

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ require_once './TiledWatermark.class.php';
 $test = TiledWatermark::getInstance();
 
 $config = array(
-     # 设置绘制类型'img'图片水印，'txt'文字水印
+	 # 设置绘制类型'img'图片水印，'txt'文字水印
      'draw_type' => 'img',
      # 背景图片，支持jpeg,png
      'draw_bg' => './resources/test.jpg',
@@ -18,15 +18,9 @@ $config = array(
      # 字体文件
      'font_file' => './resources/1.ttf',
      # 倾斜度，仅文字水印生效
-     'rotate_angle' => 22,
+     'rotate_angle' => 0,
      # 水印文字
      'watermark_text'=> '某某有限责任公司',
-     # 水平偏移量
-     'horizontal' => 0,
-     # 垂直偏移量
-     'vertical' => 0,
-     # font size
-     'font_size' => 12,
      # 水印文字颜色13同等于RGB 13,13,13
      'text_rgb' => 13,
      # 文字水印是否开启阴影
@@ -34,7 +28,7 @@ $config = array(
      # 文字水印阴影颜色
      'shadow_rgb' => '160,210,119',
      # 阴影偏移量，允许负值如-3
-     'shadow_offset' => 3,
+     'shadow_offset' => 3
 
       );
 
